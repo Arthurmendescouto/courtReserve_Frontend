473552e8c2fb67747b584fb7fd6d30ae8b3354ec
Arthur <arthurmendescouto16@gmail.com>
2025-08-21 00:46:52 -0300

    feat: Filter component complet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { ExplorarQuadrasComponent } from "./components/explorar-quadras/explorar-quadras.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ExplorarQuadrasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/explorar-quadras/explorar-quadras.component.ts
+++ src/app/components/explorar-quadras/explorar-quadras.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FiltrosComponent } from '../filtros/filtros.component';
+import { QuadraListComponent } from '../quadra-list/quadra-list.component';
 
 @Component({
   selector: 'app-explorar-quadras',
-  imports: [],
+  imports: [FiltrosComponent,QuadraListComponent],
   templateUrl: './explorar-quadras.component.html',
   styleUrl: './explorar-quadras.component.css'
 })
